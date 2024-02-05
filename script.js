0190ed901e83794ab82aec8b241b2071e682f199
Kseniia Erina <moraksu@gmail.com>
2024-02-05 23:39:31 +0400

    add menu and dropdown

--- script.js
+++ script.js
@@ -2,9 +2,22 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
+function btnFunction() {
+  document.getElementById("btnDropdown").classList.toggle("show");
+}
+
 window.onclick = function(e) {
   if (!e.target.matches('.btn__account')) {
     var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.btn__dropdown')) {
+    var myDropdown = document.getElementById("btnDropdown");
       if (myDropdown.classList.contains('show')) {
         myDropdown.classList.remove('show');
       }
