3a26057e7a5df63c4aef8b04e060d58ae880189f
Kseniia Erina <moraksu@gmail.com>
2024-02-15 18:14:57 +0400

    add single page

--- script.js
+++ script.js
@@ -71,29 +71,25 @@ function fillColor(){
   sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #f16d7f ${percent1}% , #f16d7f ${percent2}%, #dadae5 ${percent2}%)`;
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// var slideIndex = 0;
+// showSlides();
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var lines = document.getElementsByClassName("line");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < lines.length; i++) {
-    lines[i].className = lines[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  lines[slideIndex-1].className += " active";
-}
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+// }
+
+// function showSlides() {
+//     var i;
+//     var slides = document.getElementsByClassName("slide__show");
+    
+//     if (slideIndex >= slides.length) { slideIndex = 0; }    
+//     if (slideIndex < 0) { slideIndex = slides.length - 1; }
+    
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].classList.remove("active");  
+//     }
+    
+//     slides[slideIndex].classList.add("active");  
+// }
